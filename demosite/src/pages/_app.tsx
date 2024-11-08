62662f091f7a7942c4498ce16cb746a22250bade
erikjensen <erikj@my.ccsu.edu>
2024-11-07 23:41:04 -0500

    set up for website, utilized about/index.tsx in order to configure initial main page and border

---
+++ demosite/src/pages/_app.tsx
@@ -0,0 +1,16 @@
+import BaseLayout from "@/components/BaseLayout";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <BaseLayout>
+    <Component {...pageProps} />
+    
+    
+    </BaseLayout>
+
+
+
+  );
+}
